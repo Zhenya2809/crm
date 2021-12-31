cfc0852b9281bd417519caabe580b1e9ef299b41
Zhenya2809 <Zhenya2809@ukr.net>
2021-12-31 10:24:56 +0200

    test

--- src/main/java/com/crm/crm/controllers/MainController.java
+++ src/main/java/com/crm/crm/controllers/MainController.java
@@ -1,33 +1,30 @@
 package com.crm.crm.controllers;
 
-import com.crm.crm.model.ClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
-    private final ClientService clientService;
-
-    public MainController(ClientService clientService) {
-        this.clientService = clientService;
-    }
 
     @GetMapping("/")
+//    (@RequestParam(name="name", required=false, defaultValue="World") String name)
     public String home(Model model) {
         return "home";
     }
+
     @GetMapping("/price")
-    public String price(Model model){
+    public String price(Model model) {
         return "price-main";
     }
+
     @GetMapping("/about")
-    public String about(Model model){
+    public String about(Model model) {
         return "aboutUs";
     }
+
     @GetMapping("/function")
-    public String function(Model model){
+    public String function(Model model) {
         return "function";
     }
 
